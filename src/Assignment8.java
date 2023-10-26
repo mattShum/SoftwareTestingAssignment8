3243ae9cf147596aff2d4b3cbe6ea62314e1bc3a
Matt Shuman <cerotulaki@gmail.com>
2023-10-25 21:54:45 -0400

    Adding code

---
+++ src/Assignment8.java
@@ -0,0 +1,12 @@
+
+public class Assignment8 {
+
+	public static void main(String[] args) {
+		
+		int x = 1;
+		int k = 1;
+		
+		System.out.println("The result is: " + Function.Fun1(x, k));		
+	}
+
+}
---
+++ src/Function.java
@@ -0,0 +1,29 @@
+
+public class Function {
+	
+	public static int Fun1(int x, int k){
+		int i = 0;
+		int y = x + k;
+		
+		do{
+			if (x/2 < 2){
+				k = y - x;
+			}
+			else{
+				x = k - 2;
+				y = y + x;
+			}
+			k = x + y;
+			i = i + 1;
+		}while (i < x);
+		
+		if (x < 0 && y < 0){
+			k = y - x;
+		}
+		else{
+			k = y + x;
+		}
+		
+		return k;
+	}
+}
